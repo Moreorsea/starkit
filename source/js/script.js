2efa7d0ffeb9eba34c472102ae3923f7c4cb60f4
d.selyanina <daraselanina89009@gmail.com>
2020-10-14 08:41:20 +0300

    сделала указатели

--- source/js/script.js
+++ source/js/script.js
@@ -1,15 +1,15 @@
 'use strict';
 
-$('#pagepiling').slick({
-    vertical: true,
-    arrows: false,
-    dots: true,
-    swipe: true,
-    swipeToSlide: true,
-    slidesToScroll: 1,
-    touchMove: true,
-    verticalSwiping: true
-});
+// $('#pagepiling').slick({
+//     vertical: true,
+//     arrows: false,
+//     dots: true,
+//     swipe: true,
+//     swipeToSlide: true,
+//     slidesToScroll: 1,
+//     touchMove: true,
+//     verticalSwiping: true
+// });
 
 $('.screen-slider').slick({
     arrows: true,
@@ -23,3 +23,29 @@ $('.screen-slider').slick({
 //     loop:true,
 //     nav:true
 // })
+
+const $slider = $("#pagepiling");
+$slider
+	.on('init', () => {
+		mouseWheel($slider)
+	})
+	.slick({
+        dots: true,
+        arrows: false,
+		vertical: true,
+		infinite: false,
+	})
+function mouseWheel($slider) {
+	$(window).on('wheel', { $slider: $slider }, mouseWheelHandler)
+}
+function mouseWheelHandler(event) {
+	event.preventDefault()
+	const $slider = event.data.$slider
+	const delta = event.originalEvent.deltaY
+	if(delta < 0) {
+		$slider.slick('slickPrev')
+	}
+	else {
+		$slider.slick('slickNext')
+	}
+}
