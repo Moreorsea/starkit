a8f3ef9475873675775f5384ab0b723dcdbd7634
d.selyanina <daraselanina89009@gmail.com>
2020-10-15 11:44:03 +0300

    опустила текст для маководов

--- source/js/script.js
+++ source/js/script.js
@@ -1,24 +1,5 @@
 'use strict';
 
-// $('#pagepiling').slick({
-//     vertical: true,
-//     arrows: false,
-//     dots: true,
-//     swipe: true,
-//     swipeToSlide: true,
-//     slidesToScroll: 1,
-//     touchMove: true,
-//     verticalSwiping: true
-// });
-
-// $('.screen-slider').slick({
-//     arrows: true,
-//     dots: false,
-//     swipe: true,
-//     infinite: false,
-//     slidesToScroll: 1
-// });
-
 $('.screen-slider').owlCarousel({
     loop:false,
     nav:true,
